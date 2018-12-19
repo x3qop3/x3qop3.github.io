09b4457a61721f34f3679a684eba9d1e14181926
ILYA BRASHAVETS <x3qop3@gmail.com>
2018-12-19 12:51:37 +0300

    update menu

---
+++ menu/script/script.js
@@ -0,0 +1,13 @@
+menu.onclick=function Navbar(){
+    var x=document.getElementById('myNavBar')
+    if(x.className==='navBar'){
+        x.className+=' responsive';
+    }else
+    {
+        x.className='navBar';
+    }
+}
+
+
+
+
